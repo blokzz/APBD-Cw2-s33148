202df998059597c3289b82876239225bedec5ed3
Kamil Bobrzak <s33148@pjwstk.edu.pl>
2026-03-18 09:30:00 +0100

    dodano klase abstrakcyjna urzadzen

---
+++ Device.cs
@@ -0,0 +1,18 @@
+﻿namespace pracaDomowa2;
+
+public abstract class Device
+{
+    private static int _nextId;
+    private int id;
+    private string name;
+    private string model;
+
+    public Device()
+    {
+        id = _nextId++;
+    }
+    
+    
+    
+    
+}
