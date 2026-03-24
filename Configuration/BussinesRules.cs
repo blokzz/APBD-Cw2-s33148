cd7694e2254353fba8432d086f06f7dc6be9ff6a
Kamil Bobrzak <s33148@pjwstk.edu.pl>
2026-03-24 11:37:20 +0100

    Dodano stale wartosci zasad biznesowuch

---
+++ Configuration/BussinesRules.cs
@@ -0,0 +1,8 @@
+namespace PracaDomowa2;
+public static class BusinessRules
+{
+    public const int StudentRentalLimit = 2;
+    public const int EmployeeRentalLimit = 5;
+    public const decimal DailyPenaltyRate = 5.50m;
+    public const int StandardRentalPeriodDays = 14;
+}
